335b359c5ae7c226cf84f28e6e8f981cd6b68f51
DonaghEgan <donagh.egan@ucdconnect.ie>
2025-01-29 16:32:04 +0000

    tcga analysis

---
+++ analysis/scripts/cancer_cell_tcga.R
@@ -0,0 +1,187 @@
+# Title: Cancer cell-specific analysis in TCGA
+# Description: Exploring the AR and DR signatures
+# in cancer cell expression profiles from TCGA,
+# inferred using CIBSERSORTx
+# Author: Donagh
+# Date: 2025-01-22
+# Version: 1.1
+
+# Library
+library(GSVA)
+library(data.table)
+library(ggpubr)
+library(tidyverse)
+library(survminer)
+library(survMisc)
+library(ggsci)
+library(survival)
+library(matrixStats)
+library(car)
+source("src/custom_plots.R")
+
+# Global variables.
+TOP_N_GENES <- 500  # Number of top genes for each signature slice
+TMB_CUTOFF  <- 10   # TMB threshold for High vs. Low
+
+# Output file paths
+PATH_MALIG_EXP <- "data/processed/CIBERSORTxHiRes_Job44_Malignant_Window32._deseq_tcga.txt"
+PATH_DEG_HMF <- "data/processed/hmf_deseq_purity_nf_aq_deg.Rds"
+PATH_SAMPLE_META <- "data/processed/data_clinical_sample.txt"
+PATH_PATIENT_META <- "data/processed/data_clinical_patient.txt"
+PATH_CLONE_COUNTS <- "data/processed/wolf_et_al_clones.txt"
+
+# Read data
+malig_exp <- fread(PATH_MALIG_EXP) %>%
+  as.data.frame() %>%
+  column_to_rownames("GeneSymbol")
+
+# Convert '.' to '-' in column names
+colnames(malig_exp) <- gsub("\\.", "-", colnames(malig_exp))
+
+# Log2 transform
+malig_exp <- log2(malig_exp + 1)
+
+# Read in DEGs (HMF) from RDS
+deg_hmf <- readRDS(PATH_DEG_HMF)
+
+# Read sample metadata - TMB
+sample_meta <- read.delim(PATH_SAMPLE_META, skip = 4)
+
+# Read patient metadata - survival info
+patient_meta <- read.delim(PATH_PATIENT_META, header = TRUE, comment.char = "#")
+
+# Read in clone count estimates
+clone_counts <- read.delim(PATH_CLONE_COUNTS)
+
+# Filter and prepare genes
+hmf_up <- deg_hmf %>%
+  arrange(desc(stat)) %>%
+  slice(seq_len(TOP_N_GENES))
+
+hmf_down <- deg_hmf %>%
+  arrange(stat) %>%  # ascending order
+  slice(seq_len(TOP_N_GENES))
+
+# Subset malignant expression by selected gene lists
+malig_hmf_up <- malig_exp[rownames(malig_exp) %in% hmf_up$gene, ]
+malig_hmf_down <- malig_exp[rownames(malig_exp) %in% hmf_down$gene, ]
+
+# Combined Signatures
+combined_sig <- data.frame(
+  hmf_up   = colMedians(t(scale(t(malig_hmf_up))),   na.rm = TRUE),
+  hmf_down = colMedians(t(scale(t(malig_hmf_down))), na.rm = TRUE),
+  SAMPLE_ID = colnames(malig_hmf_down)
+)
+
+# Merge with sample meta data - contains TMB estimates
+sample_meta_sig <- merge(combined_sig, sample_meta, by = "SAMPLE_ID", all.x = TRUE)
+
+# Group by TMB
+sample_meta_sig$tmb_group <- ifelse(
+  sample_meta_sig$TMB_NONSYNONYMOUS < TMB_CUTOFF, "Low", "High"
+)
+
+# Remove NA values for further analysis
+sample_meta_tmb <- sample_meta_tmb %>%
+  filter(tmb_group %in% c("High", "Low"))
+
+# ITH analysis
+# format for merging
+clone_counts <- counts %>%
+  mutate(Patient = gsub("-", ".", Patient))
+
+clone_counts_sig <- merge(
+  clone_counts,
+  sample_meta_sig,
+  by.x = "Patient",
+  by.y = "PATIENT_ID",
+  all.x = TRUE  # Keep all intra_hetero rows if you want
+)
+
+# make clones numeric
+clone_counts_sig$clones <- as.numeric(clone_counts_sig$clones)
+
+# calculate cor
+hmf_cor <- cor.test(
+  x = clone_counts_sig$hmf_up,
+  y = clone_counts_sig$clones,
+  method = "spearman"
+)
+
+# Extract correlation coefficient and p-value
+rho     <- hmf_cor$estimate
+p_value <- hmf_cor$p.value
+
+# Survival Analysis
+# format for merging
+patient_meta <- gsub("-", ".", patient_meta$PATIENT_ID)
+
+# Merge - only pateints with tmb
+survial_sig <- merge(clone_counts_sig, meta_data, by.x = "Patient",
+  by.y = "PATIENT_ID")
+
+# Remove NA
+survial_sig <- survial_sig[!survial_sig$OS_MONTHS == "[Not Available]", ]
+survial_sig <- survial_sig[!is.na(survial_sig$hmf_down), ]
+survial_sig <- survial_sig[!is.na(survial_sig$tmb_group), ]
+
+# Reannotate stage variable 
+survial_sig <- survial_sig %>%
+  mutate(stage = case_when(
+    AJCC_PATHOLOGIC_TUMOR_STAGE %in% c("Stage ", "Stage IA", "Stage IB",
+    "Stage II", "Stage IIA", "Stage IIB", "Stage IIC") ~ "I_II",
+    AJCC_PATHOLOGIC_TUMOR_STAGE %in% c("Stage III", "Stage IIIA",
+    "Stage IIIB", "Stage IIIC", "Stage IV") ~ "III_IV",
+    TRUE ~ NA_character_  # Assign NA to any unmatched values for safety
+  ))
+
+# Fit Cox Model
+survial_sig$tmb_group <- relevel(as.factor(survial_sig$tmb_group), ref = "Low")
+
+cox_model <- coxph(Surv(time = as.numeric(survial_sig$OS_MONTHS),
+  event = survial_sig$OS_STATUS == "1:DECEASED") ~ stage + tmb_group + hmf_down, data = survial_sig)
+summary(cox_model)
+
+# PLOT
+colors <- pal_npg()(10)[6:7]
+
+p1 <- ggscatter(combined_sig, x = "hmf_up", y = "hmf_down",
+          add = "reg.line",
+          conf.int = TRUE,
+          cor.coef = TRUE,
+          cor.method = "spearman",
+          color = pal_npg()(4)[4],
+          add.params = list(color = "red",
+                            fill = "lightgray"),
+          size = 1.1,
+          alpha = 0.6,
+          title = "TCGA",
+          xlab = "AR Expression",
+          ylab = "NF Expression")
+
+p2 <- plot_boxplot(sample_meta_tmb, x_var = "tmb_group", y_var = "hmf_up",
+  y_label = "Exp of DR signature", comparisons = NULL,
+  x_labels = c("High", "Low"), color_pal = colors)
+
+clone_counts_sig$clones <- as.factor(clone_counts_sig$clones)
+p3 <- ggplot(clone_counts_sig, aes(x = clones, y = hmf_up, color = clones)) +
+  geom_point(position = position_jitter(width = 0.1), alpha = 0.5) +
+  theme_classic() +
+  ylab("Mean Exp NF signature") +
+  theme(legend.position = "none") +
+  stat_summary(
+    geom = "crossbar",
+    fun.y = "median",
+    col = "black",
+    size = 0.3, width = 0.35
+  ) +
+  scale_color_npg() +
+  annotate("text", x = Inf, y = Inf, label = paste("Spearman's rho =", round(rho, 2),
+  "\np-value =", format.pval(p_value, digits = 2)), 
+  hjust = 1.7, vjust = 1.1, size = 4, color = "black")
+
+pdf("analysis/output/figures/tcga_cancer_analysis.pdf", onefile = TRUE)
+p1
+p2
+p3
+dev.off()
--- analysis/scripts/tmb_hmf.R
+++ analysis/scripts/tmb_hmf.R
@@ -1,32 +1,21 @@
 # Title: Tumor Mutation Burden (TMB) Analysis and Visualization
-# Description:  This script reads TMB data and compares log10(TMB) across different outcome groups.
+# Description: This script reads TMB data and compares log10(TMB) across different outcome groups.
 # Author: Donagh
 # Date: 2025-01-28
 # Version: 1.1
 
-# ----------------------------- #
-#       Load Required Libraries #
-# ----------------------------- #
-
-library(ggplot2)      
-library(ggsci)       
-library(ggpubr)       
-library(dplyr)       
-library(readr)       
+# Load Required Libraries
+library(ggplot2)
+library(ggsci)
+library(ggpubr)
+library(dplyr)
+library(readr)
 source("src/custom_plots.R")
-
-# ----------------------------- #
-#          Define Paths          #
-# ----------------------------- #
 
 # Define relative paths for reproducibility
 tmb_file <- "data/processed/totMuts_sampleIDs.txt"
 meta_data_file <- "data/meta/meta_data.Rds"  
 counts_file <- "insert_path" ## available from the HMF. 
-
-# ----------------------------- #
-#           Load Data            #
-# ----------------------------- #
 
 # Read Tumor Mutation Burden (TMB) data
 tmb <- read_delim(tmb_file, delim = "\t", col_types = cols())
@@ -39,10 +28,7 @@ meta_data <- meta_data[rownames(meta_data) %in% colnames(counts), ]## load data
 # Define Color Palette   
 pal_colors <- rev(ggsci::pal_npg("nrc")(3)[c(2, 1, 3)])  
 
-# ----------------------------- #
-#         Data Processing        #
-# ----------------------------- #
-
+# Data Processing
 tmb_groups <- meta_data %>%
   rownames_to_column(var = "SampleID") %>%  # Add rownames as a column
   filter(group != "NA") %>%  # Filter out rows with 'NA' in 'group'
@@ -53,10 +39,6 @@ tmb_groups <- meta_data %>%
     group = factor(group, levels = c("Primary_Resistance", "Acq_Resistance", "Nofailure"))
   )
 
-# ----------------------------- #
-#    Define Statistical Tests    #
-# ----------------------------- #
-
 # Define pairwise comparisons for statistical annotations
 comparisons <- list(
   c("Acq_Resistance", "Nofailure"),
@@ -64,10 +46,7 @@ comparisons <- list(
   c("Nofailure", "Primary_Resistance")
 )
 
-# ----------------------------- #
-#         Generate Plot          #
-# ----------------------------- #
-
+#Generate Plot
 tmb_plot <- plot_boxplot(data = tmb_groups, x_var = "group", y_var = "log_TMB", 
                          y_label = "Log(10) TMB", comparisons = comparisons,
                          x_labels =  c("Primary Res", "Acquired Res", "Durable Rsp"),
